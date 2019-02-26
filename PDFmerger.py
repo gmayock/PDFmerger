2870b2875ead51683ce85740bd0fc936ae3e744c
Garrett <45615715+gmayock@users.noreply.github.com>
2019-02-26 12:36:29 -0600

    Update PDFmerger.py

--- PDFmerger.py
+++ PDFmerger.py
@@ -1,12 +1,12 @@
 import glob, os
 from PyPDF2 import PdfFileMerger
 
-input_paths = glob.glob('D:/Keep/Resume/to_merge/input/*.pdf')
+input_paths = glob.glob('filepath/input/*.pdf')
 
 merger = PdfFileMerger()
 
 for path in input_paths:
     merger.append(open(path,'rb'))
 
-with open('D:/Keep/Resume/to_merge/output/joined.pdf', 'wb') as arg:
+with open('filepath/output/merged.pdf', 'wb') as arg:
     merger.write(arg)
